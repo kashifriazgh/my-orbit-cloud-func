f1c18ad758129ef7534f0773625cdaaf0c9ff6c5
Kashif Riaz <kashifriazgh@gmail.com>
2025-07-20 10:36:46 +0500

    date issue resolved again X

--- src/cron.ts
+++ src/cron.ts
@@ -8,7 +8,7 @@ cron.schedule('* * * * *', async () => {
   const hour = now.hour();
   const minute = now.minute();
 
-  const shouldRun = hour === 10 && (minute === 33 || minute === 34);
+  const shouldRun = hour === 10 && (minute === 39 || minute === 40);
 
   if (shouldRun) {
     console.log(
